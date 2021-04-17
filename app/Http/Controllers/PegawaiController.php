349426d8c6addbf0d5426ac4ba188cb2aa942e86
LuthfilK <62377847+LuthfilK@users.noreply.github.com>
2021-04-18 05:20:51 +0700

    Update PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -12,7 +12,7 @@ class PegawaiController extends Controller
 {   
     public function index()
     {
-        $hasil = DB::table('pegawai')->distinct('id')->count('id');
+        $hasil = DB::table('pegawai2')->distinct('id')->count('id');
          
         return view('isi', ['data' => $hasil]);
     }
@@ -20,29 +20,29 @@ class PegawaiController extends Controller
     {
           // mengambil data dari table pegawai
         
-        $hasil = DB::table('pegawai')->paginate(10);
+        $hasil = DB::table('pegawai2')->paginate(10);
         return view('data-pegawai', ['data' => $hasil]);
     }
     public function simpan(Request $req)
     {
         DB::insert(
-            'insert into pegawai (nip,nama, alamat) values (?, ?, ?)',
+            'insert into pegawai2 (nip,nama, alamat) values (?, ?, ?)',
             [$req->nip, $req->nama, $req->alamat]
         );
-        $hasil = DB::table('pegawai')->paginate(10);
+        $hasil = DB::table('pegawai2')->paginate(10);
         return view('data-pegawai', ['data' => $hasil]);
     }
     public function hapus($req)
     {
         Log::info('proses hapus dengan id=' . $req);
-        DB::delete('delete from pegawai where id = ?', [$req]);
+        DB::delete('delete from pegawai2 where id = ?', [$req]);
 
-        $hasil = DB::table('pegawai')->paginate(10);
+        $hasil = DB::table('pegawai2')->paginate(10);
         return view('data-pegawai', ['data' => $hasil]);
     }
     public function ubah($req)
     {
-        $hasil = DB::select('select * from pegawai where id = ?', [$req]);
+        $hasil = DB::select('select * from pegawai2 where id = ?', [$req]);
         return view('ubah-data', ['data' => $hasil]);
     }
     public function rubah(Request $req)
@@ -50,7 +50,7 @@ class PegawaiController extends Controller
         
         Log::info($req);
         DB::update(
-            'update pegawai set ' .
+            'update pegawai2 set ' .
                 'nip=?, ' .
                 'nama=?, ' .
                 'alamat=? where id=? ',
@@ -61,13 +61,13 @@ class PegawaiController extends Controller
                 $req->id
             ]
         );
-        $hasil = DB::table('pegawai')->paginate(10);
+        $hasil = DB::table('pegawai2')->paginate(10);
         return view('data-pegawai', ['data' => $hasil]);
     }
     public function detail($req)
     {   //DB::table('pegawai')->where('id', '$req')->paginate(10);
     //$hasil = DB::table('pegawai')->where('id', '$req')->paginate(10);
-        $hasil = DB::select('select * from pegawai where id = ?', [$req]);
+        $hasil = DB::select('select * from pegawai2 where id = ?', [$req]);
 
 
         return view('detail', ['data' => $hasil]);
@@ -79,7 +79,7 @@ class PegawaiController extends Controller
         $cari = $request->cari;
  
             // mengambil data dari table pegawai sesuai pencarian data
-        $hasil = DB::table('pegawai')
+        $hasil = DB::table('pegawai2')
         ->where('nama','like',"%".$cari."%")
         ->paginate(10);
  
